c231afab59005b11d2cc70777e2e5efd31955b28
bettyzhu912 <120416658+bettyzhu912@users.noreply.github.com>
2024-10-06 16:30:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,71 @@
+# Import packages
 import streamlit as st
 
-st.title("🎈 My new app")
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.patches import Patch
+import io
+from PIL import Image, ImageDraw
+import numpy as np
+import csv
+import json
+import pandas as pd
+from torchvision import transforms
+from transformers import AutoModelForObjectDetection
+import torch
+import openai
+import os
+import fitz
+
+import qdrant_client
+from llama_index.core import SimpleDirectoryReader
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+from llama_index.core import VectorStoreIndex, StorageContext
+from llama_index.core.indices import MultiModalVectorStoreIndex
+from llama_index.core.schema import ImageDocument
+from llama_index.core.response.notebook_utils import display_source_node
+from llama_index.core.schema import ImageNode
+from llama_index.multi_modal_llms.openai import OpenAIMultiModal
+from llama_index.core.indices.multi_modal.retriever import MultiModalVectorIndexRetriever
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+output_directory_path = "converted_images"
+if not os.path.exists(output_directory_path):
+    os.makedirs(output_directory_path)
+
+# User Interface
+st.markdown("""## Professional Indemnity Insurance Underwriting System""")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "(For 2024 IFoA GIRO Presentation)"
 )
+
+def get_pdf_to_image(docs):
+    if docs is not None:
+        with fitz.open(stream=docs.read(), filetype="pdf") as pdf_file:
+            pdf_page_count = pdf_file.page_count   
+            for page_number in range(pdf_page_count):  
+                # Get the page
+                page = pdf_document[page_number]
+                # Convert the page to an image
+                pix = page.get_pixmap()
+                # Create a Pillow Image object from the pixmap
+                image = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+                # Save the image
+                image_path = os.path.join(output_dir, f'page_{i+1}.png')
+                image.save(image_path)
+    return documents
+
+def main():
+    with st.sidebar:
+        st.title("Menu:")
+        docs = st.file_uploader('Upload your document:', type="pdf")
+        if st.button("Submit & Process", key="process_button"):  # Check if API key is provided before processing
+            with st.spinner("Processing..."):
+                raw_pdf_image = get_pdf_to_image(docs)
+                #text_chunks = get_text_chunks(raw_text)
+                #get_vector_store(text_chunks)
+                st.success("Done")
+
+if __name__ == "__main__":
+    main()
