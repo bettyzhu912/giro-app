f53dbdc537e9034e99beb8656b458158077a607d
bettyzhu912 <120416658+bettyzhu912@users.noreply.github.com>
2024-10-07 13:39:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,11 +69,11 @@ def main():
     # right hand side UI configuration 
     df = pd.DataFrame(columns=['name', 'age', 'qualifications', 'date_qualified', 'numbers_of_years_in_this_capacity_with_the_proposer'])
     config = {
-        'name' : st.column_config.TextColumn('Full Name (required)', width='large', required=True),
-        'age' : st.column_config.NumberColumn('Age (years)', min_value=0, max_value=122),
-        'qualifications' : st.column_config.TextColumn('Qualifications', width='small', required=True),
+        'name' : st.column_config.TextColumn('Full Name', required=True),
+        'age' : st.column_config.NumberColumn('Age (years)', width='small', min_value=0, max_value=122),
+        'qualifications' : st.column_config.TextColumn('Qualifications', required=True),
         'date_qualified': st.column_config.DateColumn('Date Qualified', min_value=date(2000, 1, 1), max_value=date(2099, 1, 1), format="DD/MM/YYYY", step=1),
-        'numbers_of_years_in_this_capacity_with_the_proposer': st.column_config.NumberColumn('Age (years)', min_value=0, max_value=122, width='large')
+        'numbers_of_years_in_this_capacity_with_the_proposer': st.column_config.NumberColumn('# of Years in this capacity with the proposer', min_value=0, max_value=122, width='large')
     }
     st.data_editor(df, column_config = config, num_rows= "dynamic")
     # left hand side activities
