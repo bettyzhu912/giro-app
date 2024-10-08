3d1653e1a1761825610509cc040805407790a7aa
bettyzhu912 <120416658+bettyzhu912@users.noreply.github.com>
2024-10-08 13:22:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -264,7 +264,7 @@ def main():
     else:
         name_insured = st.text_input("Name under which business is conducted: (‘You’)", value = str(response_text_1), key="name_insured")
         address = st.text_input("Addresses of all of your offices & percentage of total fees in each", value = str(response_text_2), key="address")
-        commence_date = st.date_input("Date Commenced", value = datetime.strptime(str(response_text_3), "%Y-%m-%d").date(), key="commence_date")
+        commence_date = st.date_input("Date Commenced", value = datetime.strptime(str(response_text_3), "%d/%m/%Y").date(), key="commence_date")
         st.markdown("<p style='font-size:14px; color:black;'>Give details below of all Principals (including details of sole principal)</p>", unsafe_allow_html=True)
         st.data_editor(updated_df, column_config = config,  num_rows= "dynamic")
 
