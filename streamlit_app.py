6cebbc0ee875b6d001c8e04a418a64be767d3442
bettyzhu912 <120416658+bettyzhu912@users.noreply.github.com>
2024-10-08 12:03:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -204,9 +204,9 @@ def main():
     empty_directory(cropped_table_directory_path)
     
     # Prompts to feed in
-    prompt_1="based on the first image in this collection, return Name under which business is conducted: (‘You’)? Do not return other words not in the document and do not change your response if i dont change the prompt. Should be Tuhao Zhu that box"
+    prompt_1="based on the first image in this collection, return Name under which business is conducted: (‘You’)? Do not return other words not in the document and do not change your response if i dont change the prompt. "
     prompt_2="on the first image in this collection, simply return me the address with the postal code without the website. Do not return other words not in the document"
-    prompt_3="based on the first image in this collection, give full details of activities undertake and of any intended charge in these. Do not return other words not in the document and do not change your response if i dont change the prompt"
+    prompt_3="based on the first image in this collection, give full details of activities undertake and of any intended charge in these. It should be starting with sentences 'I run wembley dog park...'. Do not return other words not in the document and do not change your response if i dont change the prompt"
     prompt_4="return the information in the table: name, age, qualifications, Date qualified, Numbers of years in this capacity with the Proposer, only return the table in dictionary format (without the python in the response) with key='data', give me consistent response no matter when i ask"
     
     # Right hand side UI configuration 
